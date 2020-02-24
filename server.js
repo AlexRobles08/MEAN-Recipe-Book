40f5c45e5f897ce60e808cb15464570a3d6f5cdb
AlexRobles08 <42976847+AlexRobles08@users.noreply.github.com>
2020-02-24 11:50:11 +0100

    Added connection to the database and added the colors dependency

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Importing the routes
-const recipes = require("./routes/recipes");
+const connectDB = require("./config/db");
 
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Importing the routes
+const recipes = require("./routes/recipes");
 
 const app = express();
 
@@ -20,7 +24,14 @@ app.use("/api/v1/recipes", recipes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle undhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
